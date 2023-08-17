273d3b29b900144d17a76d57d43153fcd8fa6fd5
pmwita <pmwita2017@gmail.com>
2023-08-17 19:15:28 +0300

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 export default {
-    JWT_SECRET: "r3S3tuosVl5A8w+TMrjw0Q+FTITR7yIM+mvIjmP52FQ=",
-    EMAIL: "lew.schiller3@ethereal.email",
-    PASSWORD: "g54tsN2Npdy14GHvnJ",
-    ATLAS_URI: "mongodb+srv://pmwita2017:JdNQqVSTTc5pB8V9@cluster1.htcn8ka.mongodb.net/?retryWrites=true&w=majority"
+    JWT_SECRET: "",
+    EMAIL: "",
+    PASSWORD: "",
+    ATLAS_URI: ""
 }
